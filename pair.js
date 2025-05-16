6a38600b862dcce5c6b8b46bd731fccf8214c65a
Cobutech254pro <calvinonyango963@gmail.com>
2025-05-16 09:19:05 +0300

    𝐏𝐑𝐄𝐌𝐈𝐔𝐌
    Signed-off-by: Cobutech254pro <calvinonyango963@gmail.com>

--- pair.js
+++ pair.js
@@ -6,7 +6,7 @@ const fs = require('fs');
 let router = express.Router()
 const pino = require("pino");
 const {
-    default: Gifted_Tech,
+    default: Cobu_Indystry_Tech,
     useMultiFileAuthState,
     delay,
     makeCacheableSignalKeyStore,
@@ -16,17 +16,17 @@ const {
 function removeFile(FilePath){
     if(!fs.existsSync(FilePath)) return false;
     fs.rmSync(FilePath, { recursive: true, force: true })
- };
+   };
 router.get('/', async (req, res) => {
     const id = makeid();
     let num = req.query.number;
-        async function GIFTED_MD_PAIR_CODE() {
+        async function COBU_INDYSTRY_MD_PAIR_CODE() {
         const {
             state,
             saveCreds
         } = await useMultiFileAuthState('./temp/'+id)
-     try {
-            let Pair_Code_By_Gifted_Tech = Gifted_Tech({
+    try {
+            let Pair_Code_By_Cobu_Indystry_Tech = Cobu_Indystry_Tech({
                 auth: {
                     creds: state.creds,
                     keys: makeCacheableSignalKeyStore(state.keys, pino({level: "fatal"}).child({level: "fatal"})),
@@ -34,17 +34,17 @@ router.get('/', async (req, res) => {
                 printQRInTerminal: false,
                 logger: pino({level: "fatal"}).child({level: "fatal"}),
                 browser: ["Chrome (Linux)", "", ""]
-             });
-             if(!Pair_Code_By_Gifted_Tech.authState.creds.registered) {
+            });
+            if(!Pair_Code_By_Cobu_Indystry_Tech.authState.creds.registered) {
                 await delay(1500);
                         num = num.replace(/[^0-9]/g,'');
-                            const code = await Pair_Code_By_Gifted_Tech.requestPairingCode(num)
-                 if(!res.headersSent){
-                 await res.send({code});
-                     }
-                 }
-            Pair_Code_By_Gifted_Tech.ev.on('creds.update', saveCreds)
-            Pair_Code_By_Gifted_Tech.ev.on("connection.update", async (s) => {
+                        const code = await Pair_Code_By_Cobu_Indystry_Tech.requestPairingCode(num)
+                if(!res.headersSent){
+                await res.send({code});
+                    }
+                }
+            Pair_Code_By_Cobu_Indystry_Tech.ev.on('creds.update', saveCreds)
+            Pair_Code_By_Cobu_Indystry_Tech.ev.on("connection.update", async (s) => {
                 const {
                     connection,
                     lastDisconnect
@@ -54,11 +54,11 @@ router.get('/', async (req, res) => {
                 let data = fs.readFileSync(__dirname + `/temp/${id}/creds.json`);
                 await delay(800);
                let b64data = Buffer.from(data).toString('base64');
-               let session = await Pair_Code_By_Gifted_Tech.sendMessage(Pair_Code_By_Gifted_Tech.user.id, { text: '' + b64data });
+               let session = await Pair_Code_By_Cobu_Indystry_Tech.sendMessage(Cobu_Indystry_Tech.user.id, { text: '' + b64data });
 
-               let GIFTED_MD_TEXT = `
+               let COBU_INDYSTRY_MD_TEXT = `
 ┏━━━━━━━━━━━━━━
-┃QUEEN_ANITA_V2 SESSION IS 
+┃QUEEN_ANITA_V2 SESSION IS
 ┃SUCCESSFULLY
 ┃CONNECTED ✅🔥
 ┗━━━━━━━━━━━━━━━
@@ -73,25 +73,15 @@ Wanna talk to me?👉 https://t.me/deecee_x 👈
 ©*2024-2099 David Cyril*
 
 _Don't Forget To Give Star To My Repo_`
- await Pair_Code_By_Gifted_Tech.sendMessage(Pair_Code_By_Gifted_Tech.user.id,{text:GIFTED_MD_TEXT},{quoted:session})
- 
+ await Pair_Code_By_Cobu_Indystry_Tech.sendMessage(Cobu_Indystry_Tech.user.id,{text:COBU_INDYSTRY_MD_TEXT},{quoted:session})
+
 
         await delay(100);
-        await Pair_Code_By_Gifted_Tech.ws.close();
+        await Cobu_Indystry_Tech.ws.close();
         return await removeFile('./temp/'+id);
-            } else if (connection === "close" && lastDisconnect && lastDisconnect.error && lastDisconnect.error.output.statusCode != 401) {
-                    await delay(10000);
-                    GIFTED_MD_PAIR_CODE();
-                }
-            });
-        } catch (err) {
-            console.log("service restated");
-            await removeFile('./temp/'+id);
-         if(!res.headersSent){
-            await res.send({code:"Service Unavailable"});
-         }
-        }
-    }
-    return await GIFTED_MD_PAIR_CODE()
-});
-module.exports = router
+                } else if (connection === "close" && lastDisconnect && lastDisconnect.error && lastDisconnect.error.output.statusCode != 401) {
+                        await delay(10000);
+                        COBU_INDYSTRY_MD_PAIR_CODE();
+                    }
+                });
+        } catch (
